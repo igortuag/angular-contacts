ce79669e2cc82114a069753e07110a2e4d53aa2a
Tuag <igortuag@gmail.com>
2022-01-25 17:31:27 -0300

    config routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,9 @@
 import { ModuleWithProviders } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { CoursesComponent } from "./courses/courses.component";
 
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
+import { CoursesComponent } from "./courses/courses.component";
 
 const APP_ROUTES: Routes = [
   { path: "", component: HomeComponent },
