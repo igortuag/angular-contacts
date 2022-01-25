6ffcfff1cc85805ca5da0f01868be63eddd0ed19
Tuag <igortuag@gmail.com>
2022-01-25 18:22:48 -0300

    add ActivatedRoute as dependence injection

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-course-detail',
-  templateUrl: './course-detail.component.html',
-  styleUrls: ['./course-detail.component.css']
+  selector: "app-course-detail",
+  templateUrl: "./course-detail.component.html",
+  styleUrls: ["./course-detail.component.css"],
 })
 export class CourseDetailComponent implements OnInit {
+  id: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {
+    this.id = this.route.snapshot.params["id"];
   }
 
+  ngOnInit() {}
 }
