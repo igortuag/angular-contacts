75dce8950270e2eeea0dbe4a7cacf47d17212d3e
Tuag <igortuag@gmail.com>
2022-01-26 13:52:31 -0300

    add method to get courses

--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class CoursesService {
+  getCourses() {
+    return [
+      {
+        id: 1,
+        name: "Angular 2",
+      },
+      {
+        id: 2,
+        name: "React",
+      },
+      {
+        id: 3,
+        name: "Vue",
+      },
+    ];
+  }
 
-  constructor() { }
-
+  constructor() {}
 }
